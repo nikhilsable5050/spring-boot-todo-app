9e2687203bd5ede43142ca16bafd936e2bcd211b
nikhisable5050 <nikhilsable5050@gmail.com>
2026-01-22 20:13:29 +0530

    Initial commit: Spring Boot Todo App setup

---
+++ src/test/java/com/nikhilsable/todoapp/TodoappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nikhilsable.todoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
