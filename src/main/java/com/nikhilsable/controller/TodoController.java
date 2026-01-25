6754362d7fc32b52cf44be60e46ae7065841f026
nikhisable5050 <nikhilsable5050@gmail.com>
2026-01-25 20:51:42 +0530

    Finalize Todo entity and controller with title-based todo creation

--- src/main/java/com/nikhilsable/controller/TodoController.java
+++ src/main/java/com/nikhilsable/controller/TodoController.java
@@ -1,10 +1,14 @@
 package com.nikhilsable.controller;
 
+import com.nikhilsable.entity.TodoEntity;
 import com.nikhilsable.repository.TodoRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequiredArgsConstructor
@@ -16,5 +20,33 @@ public class TodoController {
     public String showHomePage(Model model) {
         model.addAttribute("todos", todoRepository.findAll());
         return "index";
+    }
+
+    @PostMapping("/add")
+    public String add(@RequestParam String title) {
+        TodoEntity newTodo = TodoEntity.builder()
+                .title(title)
+                .completed(false)
+                .build();
+            todoRepository.save(newTodo);
+        return "redirect:/";
+    }
+
+    @GetMapping("/update/{id}")
+    public String update(@PathVariable Long id) {
+        TodoEntity existingTodo = todoRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Todo not found: "+id));
+        existingTodo.setCompleted(true);
+        todoRepository.save(existingTodo);
+        return "redirect:/";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String delete(@PathVariable Long id) {
+        TodoEntity existingTodo = todoRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Todo not found: "+id));
+        todoRepository.delete(existingTodo);
+        return "redirect:/";
+
     }
 }
